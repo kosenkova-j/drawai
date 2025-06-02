5acdaf3fcba1b4ce454cbc776a97602bce32f419
kosenkova-j <kojulev@gmail.com>
2025-06-03 00:50:30 +0700

    работа с MainActivity

--- app/src/main/java/com/example/drawai/MainActivity.kt
+++ app/src/main/java/com/example/drawai/MainActivity.kt
@@ -1,20 +1,26 @@
 package com.example.drawai
 
+import android.content.Intent
 import android.os.Bundle
-import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
-import androidx.core.view.ViewCompat
-import androidx.core.view.WindowInsetsCompat
+import androidx.recyclerview.widget.GridLayoutManager
+import dagger.hilt.android.AndroidEntryPoint
 
+@AndroidEntryPoint
 class MainActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityGalleryBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
-        setContentView(R.layout.activity_main)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
-            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
-            insets
+        binding = ActivityGalleryBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        setupFab()
+    }
+
+    private fun setupFab() {
+        binding.fabAdd.setOnClickListener {
+            startActivity(Intent(this, DrawingActivity::class.java))
         }
     }
 }
