9907529b812d0fd03f12efda2c98a9eccd6fae82
kosenkova-j <kojulev@gmail.com>
2025-06-03 11:54:42 +0700

    работа с api

--- app/src/main/java/com/example/drawai/api/ArtApi.kt
+++ app/src/main/java/com/example/drawai/api/ArtApi.kt
@@ -1,23 +1,21 @@
 package com.example.drawai.api
 
-import com.google.gson.annotations.SerializedName
+import retrofit2.Response
 import retrofit2.http.Body
+import retrofit2.http.Header
 import retrofit2.http.POST
 
 interface ArtApi {
-    @POST("api/v1/art-generator")
-    suspend fun generateArt(
-        @Body request: String
-    ): ArtGenerationResponse
+    companion object {
+        const val BASE_URL = "https://api.stability.ai/"
+        const val API_KEY = "Bearer sk-zeepJc7LBeM38XUDrStRtODcyfpU65oA72Mba1BxK4WCDfci"
+    }
 
-    // Add other API endpoints as needed
+    @POST("v1/generation/stable-diffusion-v1-6/image-to-image")
+    suspend fun generateImage(
+        @Header("Authorization") auth: String = API_KEY,
+        @Header("Content-Type") contentType: String = "application/json",
+        @Header("Accept") accept: String = "application/json",
+        @Body request: StableDiffusionRequest
+    ): Response<StableDiffusionResponse>
 }
-
-data class ArtGenerationRequest(
-    @SerializedName("image") val image: String,
-    @SerializedName("style") val style: String
-)
-
-data class ArtGenerationResponse(
-    @SerializedName("generated_image") val generatedImage: String
-)
